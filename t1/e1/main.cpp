fc364537a8618db07650c44ba42f183da323b058
toufik <toufik@forgia.fr>
2021-06-09 11:21:20 +0200

    a

--- t1/e1/main.cpp
+++ t1/e1/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
-
 using namespace std;
+
+
+int ajout(int a)
+{
+    a+=2;
+    return a;
+}
 
 int main()
 {
-    int ageUser = 31, ageU(32);
-
-
+    int ageUser = 31, ageU(32), result;
     cout << "Bismillah !" << endl << ageUser << endl << ageU;
+
+    result = ajout(ageUser);
+
+    cout << "Age : " << result;
 
 
     return 0;
